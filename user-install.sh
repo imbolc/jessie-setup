f00c28b8b2010288c4ee43eeeaeb6110a35e23ad
imbolc <imbolc@imbolc.name>
2017-01-01 23:14:22 +0700

    Up vim setup

--- user-install.sh
+++ user-install.sh
@@ -11,7 +11,8 @@ git clone https://github.com/imbolc/.vim ~/.vim
 ln -s ~/.vim/.vimrc ~
 ln -s ~/.vim ~/.config/nvim
 
-update-alternatives --set editor /usr/bin/nvim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
 nvim +PlugInstall +qall
 
 
